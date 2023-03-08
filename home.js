037edec0d8e8ad376fbc2b3dd063cf1e2d79669a
annaquainliu <annaquainliu@gmail.com>
2023-03-08 01:45:04 -0500

    Added slideshow background

--- home.js
+++ home.js
@@ -1,25 +1,28 @@
-const background = document.getElementById("background");
-const images = ["R.jpg", "blizzard.jpg"];
-var imageIndex = 0;
-
-function addAnimation() {
-    background.style.animationName = "slide";
-    background.style.animationDuration = "5s";
-    background.style.animationIterationCount = "infinite";
-    background.style.backgroundImage = `url('images/${images[imageIndex]}')`;
-}
-
-function startAnimation() {
-    setInterval(() => {
-        imageIndex++;
-        if (imageIndex == images.length) {
-            imageIndex = 0;
-        }
-        background.style.backgroundImage = `url('images/${images[imageIndex]}')`;
-    } , 5000);
-}
-
 window.onload = () => {
+    const background = document.getElementById("background");
+    const images = ["R.jpg", "blizzard.jpg"];
+    var imageIndex = 0;
+
+    function addAnimation() {
+        background.style.animationName = "slide";
+        background.style.animationDuration = "5s";
+        background.style.animationIterationCount = "infinite";
+        background.style.backgroundImage = `url('images/${images[imageIndex]}')`;
+    }
+
+    function startAnimation() {
+        setInterval(() => {
+            imageIndex++;
+            if (imageIndex == images.length) {
+                imageIndex = 0;
+            }
+            background.style.backgroundImage = `url('images/${images[imageIndex]}')`;
+        } , 5000);
+    }
+
     addAnimation();
     startAnimation();
 }
+
+
+
