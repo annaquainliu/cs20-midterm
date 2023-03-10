e11b8430912c98b55239f367aeeb659f08d0b2b3
annaquainliu <annaquainliu@gmail.com>
2023-03-09 20:48:19 -0500

    Added some banger comments

--- forum.js
+++ forum.js
@@ -1,3 +1,19 @@
 window.onload = () => {
-    
+    const likes = document.getElementsByClassName('likes');
+    Array.prototype.forEach.call(likes, like => {
+        const heartDiv = like.children[0];
+        const likeVal = like.children[1];
+        heartDiv.addEventListener('click', () => {
+            let value = parseInt(likeVal.innerText);
+            heartDiv.liked = !heartDiv.liked;
+            if (heartDiv.liked) {
+                heartDiv.style.backgroundImage = 'url(images/heart.png)';
+                likeVal.innerText = value + 1;
+            }
+            else {
+                heartDiv.style.backgroundImage = 'url(images/heartUnliked.png)';
+                likeVal.innerText = value - 1;
+            }
+        });
+    });
 }
