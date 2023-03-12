dc1974033e645e3929295cae9569b730c53e0de3
swj20010308 <swj20010308@outlook.com>
2023-03-12 05:38:57 -0400

    full working animation links to be added

--- game_store.js
+++ game_store.js
@@ -1,8 +1,10 @@
 function display (games, curr) {
     for (i = 0; i < games.length; i++) {
-        games[i].style.display = 'none';
+        if (games[i].style.display == 'block') {
+            crossFade(games[i], games[curr]);
+            break;
+        }
     }
-    games[curr].style.display = 'block';
 }
 
 function nextGame (games, index) {
@@ -18,3 +20,18 @@ function prevGame (games, index) {
     }
     return index;
 }
+
+function crossFade (curr, next) {
+    $(next).css({'display': 'block', 'opacity': '0%'});
+    $(curr).animate({opacity: '0%'}, {duration: 200, queue: false});
+    $(next).animate({opacity: '100%'}, {duration: 200, queue: false});
+    $(curr).hide();
+}
+
+function changeBackGround (images, to) {
+    $background = $('#background');
+    $background.animate({opacity: '0%'}, {duration: 100, queue: true});
+    $background.css('background-image', `url('images/${images[to]}')`);
+    $background.animate({opacity: '10%'}, {duration: 100, queue: true});
+}
+
