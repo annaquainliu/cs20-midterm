ded0d178b7758593da24dad10210cecbf65976d9
swj20010308 <swj20010308@outlook.com>
2023-03-12 21:16:36 -0400

    game_store layout finished. add text needed

--- game_store.js
+++ game_store.js
@@ -35,3 +35,27 @@ function changeBackGround (images, to) {
     $background.animate({opacity: '10%'}, {duration: 100, queue: true});
 }
 
+function toggleHeight (elem, fstClick, height) {
+    if (fstClick) {
+        $(elem).css('display', 'flex');
+        $(elem).animate({opacity: '100%', height: height}, {duration: 200, queue: true})
+    } else {
+        $(elem).animate({opacity: "0%", height: '0px'}, {duration: 200, queue: true})
+        $(elem).delay(200).hide(0);
+    }
+    return !fstClick;
+}
+
+function toggleWidth (elem, fstClick) {
+    if (fstClick) {
+        $(elem).css('display', 'block');
+        $(elem).animate({opacity: '100%', width: ($(elem).parent().width() / 2) }, {duration: 200, queue: true})
+    } else {
+        $(elem).animate({opacity: "0%", width: '0px'}, {duration: 200, queue: true})
+        $(elem).delay(200).hide(0);
+    }
+    return !fstClick;
+}
+
+
+
